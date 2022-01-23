fa34317aa70a773b42a505568efd4fc0fb86d9c6
Josh H (santiago) <hendershotjoshr@gmail.com>
2022-01-23 06:58:14 -0400

    added some controller stuff

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
 from librosa import note_to_hz as hz
+from pygame import MIDIIN
 
 from composers import Chain
 from modifiers import ModulatedPanner
 from oscillators import SineOscillator, TriangleOscillator
-from utils import gettrig, wave_to_file
+from utils import get_samples, gettrig, wave_to_file
+
+
+def main():
+    MIDIIN.init()
+    default_id = MIDIIN.get_default_input_id()
+    midi_input = MIDIIN.Input(device_id=default_id)
+
+    stream = pyaudio.PyAudio().open(
+        rate=44100,
+        channels=1,
+        format=pyaudio.paInt16,
+        output=True,
+        frames_per_buffer=256,
+    )
+
+    try:
+        notes_dict = {}
+        while True:
+            if notes_dict:
+                # Play the notes
+                samples = get_samples(notes_dict)
+                samples = np.int16(samples).tobytes()
+                stream.write(samples)
+
+            if midi_input.poll():
+                # Add or remove notes from notes_dict
+                for event in midi_input.read(num_events=16):
+                    (status, note, vel, _), _ = event
+                    if status == 0x80 and note in notes_dict:
+                        del notes_dict[note]
+                    elif status == 0x90 and note not in notes_dict:
+                        freq = midi.midi_to_frequency(note)
+                        notes_dict[note] = get_sin_oscillator(freq=freq, amp=vel / 127)
+
+    except KeyboardInterrupt as err:
+        print("Stopping...")
 
 
 # osc = SineOscillator()
 # wav = get_seq(osc)
 # wave_to_file(wav, fname="c4_maj_sine.wav")
 
-gen = Chain(
-    SineOscillator(220), ModulatedPanner(SineOscillator(4, wave_range=(-0.8, 0.8)))
-)
-wav = gettrig(gen, 4)
-wave_to_file(wav, fname="idk.wav")
+# gen = Chain(
+#     SineOscillator(220), ModulatedPanner(SineOscillator(4, wave_range=(-0.8, 0.8)))
+# )
+# wav = gettrig(gen, 4)
+# wave_to_file(wav, fname="idk.wav")
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,8 +1,22 @@
+import itertools
+import math
 import librosa
 import numpy as np
 from scipy.io import wavfile
 
 SR = 44_100
+
+
+def get_sin_oscillator(freq=55, amp=1, sample_rate=44100):
+    increment = (2 * math.pi * freq) / sample_rate
+    return (math.sin(v) * amp for v in itertools.count(start=0, step=increment))
+
+
+def get_samples(notes_dict, num_samples=256):
+    return [
+        sum([int(next(osc) * 32767) for _, osc in notes_dict.items()])
+        for _ in range(num_samples)
+    ]
 
 
 def get_val(osc, sample_rate=SR):
